660db6ef3e1883de58296abb55f4cf3c9ce9a6c1
Haira Herrmann <111297087+HairaLabress@users.noreply.github.com>
2023-06-06 14:34:12 -0300

    Create multiplicador.py

---
+++ multiplicador.py
@@ -0,0 +1,27 @@
+#Crie funções que duplicam, triplicam e quadruolicam o valor do numero
+
+
+def conta(numero, operacao):
+    calculo = numero * operacao
+    return calculo
+    
+    
+numero = conta(2, 4)
+print(numero)
+
+"""
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadruplicar(2))
+
+"""
